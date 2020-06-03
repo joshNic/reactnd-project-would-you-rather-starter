779aa6af8c5963689917b9b50c57fe8e9d3627e3
joshNic <256jomu@gmail.com>
2020-06-03 13:00:53 +0300

    modify readme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-// import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page from "./components/Page";
 import Question from "./components/Question";
 import LeaderBoard from "./components/LeaderBoard";
 import Login from "./components/Login";
-import AnswerPoll from './components/AnswerPoll'
-import PollResult from './components/PollResult'
+
 import { handleInitialData } from "./actions/shared";
 import { connect } from "react-redux";
-import NotFound from './components/NotFound'
 
 class App extends React.Component {
   componentDidMount() {
@@ -28,16 +24,11 @@ class App extends React.Component {
           </div>
         ) : (
           <Switch>
-            {/* <Route exact path="/login" component={Login} /> */}
             <Page>
-              
-              {/* <Route component={NotFound} /> */}
               <Route exact path="/" component={Home} />
               <Route  exact path="/add" component={Question} />
               <Route exact path="/leaderboard" component={LeaderBoard} />
-              <Route exact path="/questions/:question_id" component={AnswerPoll} />
-              {/* <Route exact path="/result/:question_id" component={PollResult} /> */}
-              <Route exact path="/not_found" component={NotFound} />
+              {/* <Route exact path="/not_found" component={NotFound} /> */}
             </Page>
           </Switch>
         )}
